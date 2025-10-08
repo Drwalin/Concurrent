50e0d464f45b9604a8699ac8aa0b6b98010d978d
Drwalin <34726800+Drwalin@user.noreply.github.com>
2025-10-09 01:30:14 +0200

    Improve usability, make global scope arithmetic and constexpr of concurrent::time::* functions.

--- time.hpp
+++ time.hpp
@@ -14,6 +14,7 @@ namespace time
 {
 struct diff {
 	int64_t ns = 0;
+	inline constexpr diff operator-() const { return {-ns}; }
 };
 
 struct point {
@@ -23,76 +24,85 @@ struct point {
 point now();
 void sleep_for(diff dt);
 
-inline diff nanoseconds(int t) { return {t}; }
-inline diff microseconds(int t) { return {t * 1000ll}; }
-inline diff milliseconds(int t) { return {t * 1000000ll}; }
-inline diff seconds(int t) { return {t * 1000000000ll}; }
+inline constexpr diff abs(diff t) { return t.ns < 0 ? diff{-t.ns} : t; }
+inline constexpr diff clamp(diff t, diff min, diff max) { return t.ns < min.ns ? min : t.ns > max.ns ? max : t; }
+inline constexpr diff min(diff a, diff b) { return a.ns < b.ns ? a : b; }
+inline constexpr diff max(diff a, diff b) { return a.ns > b.ns ? a : b; }
 
-inline diff nanoseconds(int64_t t) { return {t}; }
-inline diff microseconds(int64_t t) { return {t * 1000ll}; }
-inline diff milliseconds(int64_t t) { return {t * 1000000ll}; }
-inline diff seconds(int64_t t) { return {t * 1000000000ll}; }
+inline constexpr diff nanoseconds(int t)  { return {t}; }
+inline constexpr diff microseconds(int t) { return {t * 1000ll}; }
+inline constexpr diff milliseconds(int t) { return {t * 1000000ll}; }
+inline constexpr diff seconds(int t)      { return {t * 1000000000ll}; }
 
-inline diff nanoseconds(unsigned int t) { return {t}; }
-inline diff microseconds(unsigned int t) { return {t * 1000ll}; }
-inline diff milliseconds(unsigned int t) { return {t * 1000000ll}; }
-inline diff seconds(unsigned int t) { return {t * 1000000000ll}; }
+inline constexpr diff nanoseconds(int64_t t)  { return {t}; }
+inline constexpr diff microseconds(int64_t t) { return {t * 1000ll}; }
+inline constexpr diff milliseconds(int64_t t) { return {t * 1000000ll}; }
+inline constexpr diff seconds(int64_t t)      { return {t * 1000000000ll}; }
 
-inline diff nanoseconds(uint64_t t) { return {(int64_t)t}; }
-inline diff microseconds(uint64_t t) { return {int64_t(t * 1000ll)}; }
-inline diff milliseconds(uint64_t t) { return {int64_t(t * 1000000ll)}; }
-inline diff seconds(uint64_t t) { return {int64_t(t * 1000000000ll)}; }
+inline constexpr diff nanoseconds(unsigned int t)  { return {t}; }
+inline constexpr diff microseconds(unsigned int t) { return {t * 1000ll}; }
+inline constexpr diff milliseconds(unsigned int t) { return {t * 1000000ll}; }
+inline constexpr diff seconds(unsigned int t)      { return {t * 1000000000ll}; }
 
-inline diff microseconds(double t) { return {int64_t(t * 1000.0)}; }
-inline diff milliseconds(double t) { return {int64_t(t * 1000000.0)}; }
-inline diff seconds(double t) { return {int64_t(t * 1000000000.0)}; }
+inline constexpr diff nanoseconds(uint64_t t)  { return {(int64_t)t}; }
+inline constexpr diff microseconds(uint64_t t) { return {int64_t(t * 1000ll)}; }
+inline constexpr diff milliseconds(uint64_t t) { return {int64_t(t * 1000000ll)}; }
+inline constexpr diff seconds(uint64_t t)      { return {int64_t(t * 1000000000ll)}; }
 
-inline diff microseconds(float t) { return {int64_t(t * 1000.0f)}; }
-inline diff milliseconds(float t) { return {int64_t(t * 1000000.0f)}; }
-inline diff seconds(float t) { return {int64_t(t * 1000000000.0f)}; }
+inline constexpr diff microseconds(double t) { return {int64_t(t * 1000.0)}; }
+inline constexpr diff milliseconds(double t) { return {int64_t(t * 1000000.0)}; }
+inline constexpr diff seconds(double t)      { return {int64_t(t * 1000000000.0)}; }
 
-inline bool operator<(diff l, diff r) { return l.ns < r.ns; }
-inline bool operator<=(diff l, diff r) { return l.ns <= r.ns; }
-inline bool operator>(diff l, diff r) { return l.ns > r.ns; }
-inline bool operator>=(diff l, diff r) { return l.ns >= r.ns; }
-inline bool operator==(diff l, diff r) { return l.ns == r.ns; }
-inline bool operator!=(diff l, diff r) { return l.ns != r.ns; }
-
-inline bool operator<(point l, point r) { return l.ns < r.ns; }
-inline bool operator<=(point l, point r) { return l.ns <= r.ns; }
-inline bool operator>(point l, point r) { return l.ns > r.ns; }
-inline bool operator>=(point l, point r) { return l.ns >= r.ns; }
-inline bool operator==(point l, point r) { return l.ns == r.ns; }
-inline bool operator!=(point l, point r) { return l.ns != r.ns; }
-
-inline point operator+(point p, diff d) { return {p.ns + d.ns}; }
-inline point operator-(point p, diff d) { return {p.ns - d.ns}; }
-
-inline point &operator+=(point &p, diff d) { p.ns += d.ns; return p; }
-inline point &operator-=(point &p, diff d) { p.ns -= d.ns; return p; }
-
-inline diff operator-(point a, point b) { return {a.ns - b.ns}; }
-inline diff operator-(diff a, diff b) { return {a.ns - b.ns}; }
-inline diff operator+(diff a, diff b) { return {a.ns + b.ns}; }
-
-inline diff &operator+=(diff &a, diff b) { a.ns += b.ns; return a; }
-inline diff &operator-=(diff &a, diff b) { a.ns -= b.ns; return a; }
-
-inline diff operator*(diff a, int v) { return {a.ns * v}; }
-inline diff operator/(diff a, int v) { return {a.ns / v}; }
-inline diff operator*(diff a, float v) { return {int64_t(a.ns * v)}; }
-inline diff operator/(diff a, float v) { return {int64_t(a.ns / v)}; }
-inline diff operator*(diff a, double v) { return {int64_t(a.ns * v)}; }
-inline diff operator/(diff a, double v) { return {int64_t(a.ns / v)}; }
-
-inline diff &operator*=(diff &a, int v)    { a.ns *= v; return a; }
-inline diff &operator/=(diff &a, int v)    { a.ns /= v; return a; }
-inline diff &operator*=(diff &a, float v)  { a.ns *= v; return a; }
-inline diff &operator/=(diff &a, float v)  { a.ns /= v; return a; }
-inline diff &operator*=(diff &a, double v) { a.ns *= v; return a; }
-inline diff &operator/=(diff &a, double v) { a.ns /= v; return a; }
+inline constexpr diff microseconds(float t) { return {int64_t(t * 1000.0f)}; }
+inline constexpr diff milliseconds(float t) { return {int64_t(t * 1000000.0f)}; }
+inline constexpr diff seconds(float t)      { return {int64_t(t * 1000000000.0f)}; }
 
 } // namespace time
 } // namespace concurrent
+
+inline constexpr bool operator< (concurrent::time::diff l, concurrent::time::diff r) { return l.ns < r.ns; }
+inline constexpr bool operator<=(concurrent::time::diff l, concurrent::time::diff r) { return l.ns <= r.ns; }
+inline constexpr bool operator> (concurrent::time::diff l, concurrent::time::diff r) { return l.ns > r.ns; }
+inline constexpr bool operator>=(concurrent::time::diff l, concurrent::time::diff r) { return l.ns >= r.ns; }
+inline constexpr bool operator==(concurrent::time::diff l, concurrent::time::diff r) { return l.ns == r.ns; }
+inline constexpr bool operator!=(concurrent::time::diff l, concurrent::time::diff r) { return l.ns != r.ns; }
+
+inline constexpr bool operator< (concurrent::time::point l, concurrent::time::point r) { return l.ns < r.ns; }
+inline constexpr bool operator<=(concurrent::time::point l, concurrent::time::point r) { return l.ns <= r.ns; }
+inline constexpr bool operator> (concurrent::time::point l, concurrent::time::point r) { return l.ns > r.ns; }
+inline constexpr bool operator>=(concurrent::time::point l, concurrent::time::point r) { return l.ns >= r.ns; }
+inline constexpr bool operator==(concurrent::time::point l, concurrent::time::point r) { return l.ns == r.ns; }
+inline constexpr bool operator!=(concurrent::time::point l, concurrent::time::point r) { return l.ns != r.ns; }
+
+inline constexpr concurrent::time::point operator+(concurrent::time::point p, concurrent::time::diff d) { return {p.ns + d.ns}; }
+inline constexpr concurrent::time::point operator-(concurrent::time::point p, concurrent::time::diff d) { return {p.ns - d.ns}; }
+
+inline constexpr concurrent::time::point &operator+=(concurrent::time::point &p, concurrent::time::diff d) { p.ns += d.ns; return p; }
+inline constexpr concurrent::time::point &operator-=(concurrent::time::point &p, concurrent::time::diff d) { p.ns -= d.ns; return p; }
+
+inline constexpr concurrent::time::diff operator-(concurrent::time::point a, concurrent::time::point b) { return {a.ns - b.ns}; }
+inline constexpr concurrent::time::diff operator-(concurrent::time::diff a, concurrent::time::diff b)   { return {a.ns - b.ns}; }
+inline constexpr concurrent::time::diff operator+(concurrent::time::diff a, concurrent::time::diff b)   { return {a.ns + b.ns}; }
+
+inline constexpr concurrent::time::diff &operator+=(concurrent::time::diff &a, concurrent::time::diff b) { a.ns += b.ns; return a; }
+inline constexpr concurrent::time::diff &operator-=(concurrent::time::diff &a, concurrent::time::diff b) { a.ns -= b.ns; return a; }
+
+inline constexpr concurrent::time::diff operator*(concurrent::time::diff a, int v)     { return {a.ns * v}; }
+inline constexpr concurrent::time::diff operator/(concurrent::time::diff a, int v)     { return {a.ns / v}; }
+inline constexpr concurrent::time::diff operator*(concurrent::time::diff a, int64_t v) { return {a.ns * v}; }
+inline constexpr concurrent::time::diff operator/(concurrent::time::diff a, int64_t v) { return {a.ns / v}; }
+inline constexpr concurrent::time::diff operator*(concurrent::time::diff a, float v)   { return {int64_t(a.ns * v)}; }
+inline constexpr concurrent::time::diff operator/(concurrent::time::diff a, float v)   { return {int64_t(a.ns / v)}; }
+inline constexpr concurrent::time::diff operator*(concurrent::time::diff a, double v)  { return {int64_t(a.ns * v)}; }
+inline constexpr concurrent::time::diff operator/(concurrent::time::diff a, double v)  { return {int64_t(a.ns / v)}; }
+
+inline constexpr concurrent::time::diff &operator*=(concurrent::time::diff &a, int v)     { a.ns *= v; return a; }
+inline constexpr concurrent::time::diff &operator/=(concurrent::time::diff &a, int v)     { a.ns /= v; return a; }
+inline constexpr concurrent::time::diff &operator*=(concurrent::time::diff &a, int64_t v) { a.ns *= v; return a; }
+inline constexpr concurrent::time::diff &operator/=(concurrent::time::diff &a, int64_t v) { a.ns /= v; return a; }
+inline constexpr concurrent::time::diff &operator*=(concurrent::time::diff &a, float v)   { a.ns *= v; return a; }
+inline constexpr concurrent::time::diff &operator/=(concurrent::time::diff &a, float v)   { a.ns /= v; return a; }
+inline constexpr concurrent::time::diff &operator*=(concurrent::time::diff &a, double v)  { a.ns *= v; return a; }
+inline constexpr concurrent::time::diff &operator/=(concurrent::time::diff &a, double v)  { a.ns /= v; return a; }
 
 #endif
